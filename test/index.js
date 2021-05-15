5baa17c1dd27495f83d254eab899d6696a68baee
TheComputerM <mudit.harshit99@outlook.com>
2021-05-15 15:30:27 +0530

    update testing

--- test/index.js
+++ test/index.js
@@ -1,4 +1,3 @@
-const glob = require("tiny-glob/sync");
 const path = require("path");
 const { readFileSync } = require("fs");
 
@@ -8,26 +7,50 @@ import tsdoc from "../src/typescript";
 import { test } from "uvu";
 import * as assert from "uvu/assert";
 
-glob("**/*.svelte", { cwd: path.resolve("./test/fixtures") }).forEach(
-  (file) => {
-    const name = path.join(__dirname, 'fixtures', file);
-    const input = JSON.stringify(doc({ file: name }), null, 2);
-    const output = readFileSync(name.replace(".svelte", ".json")).toString();
-    test(file, () => {
-      assert.fixture(input, output);
-    });
-  }
-);
+const files = [
+  "events/dispatch",
+  "events/forwarding",
+  "events/not.forward",
+  "module/basic",
+  "others/basic",
+  "others/bad.style.tag",
+  "props/another.var",
+  "props/change.name",
+  "props/const",
+  "props/export.notfound",
+  "props/function",
+  "props/let",
+  "props/multiple.export",
+  "props/no.default",
+  "props/singleline.comment",
+  "props/tags",
+  "restProps/custom",
+  "restProps/default",
+  "slots/default",
+  "slots/named",
+  "slots/props",
+  "styles/basic",
+  "typedef/basic",
+];
 
-glob("**/*.svelte", { cwd: path.resolve("./test/ts_fixtures") }).forEach(
-  (file) => {
-    const name = path.join(__dirname, "ts_fixtures", file);
-    const input = JSON.stringify(tsdoc({ file: name }), null, 2);
-    const output = readFileSync(name.replace(".svelte", ".json")).toString();
-    test(file, () => {
-      assert.fixture(input, output);
-    });
-  }
-);
+const tsfiles = ["basic"];
+
+files.forEach((file) => {
+  const name = path.join(__dirname, "fixtures", `${file}.svelte`);
+  const input = JSON.stringify(doc({ file: name }), null, 2);
+  const output = readFileSync(name.replace(".svelte", ".json")).toString();
+  test(file, () => {
+    assert.fixture(input, output);
+  });
+});
+
+tsfiles.forEach((file) => {
+  const name = path.join(__dirname, "ts_fixtures", `${file}.svelte`);
+  const input = JSON.stringify(tsdoc({ file: name }), null, 2);
+  const output = readFileSync(name.replace(".svelte", ".json")).toString();
+  test(file, () => {
+    assert.fixture(input, output);
+  });
+});
 
 test.run();
